0ac3bc651eda225a0646c335c08ec2c1f412bef8
paulkoup <paulkoup@gmail.com>
2026-03-12 23:53:51 -0400

    Initial Commit

---
+++ Motive/MotiveApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotiveApp.swift
+//  Motive
+//
+//  Created by Paul Koupiaris on 3/12/26.
+//
+
+import SwiftUI
+
+@main
+struct MotiveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
